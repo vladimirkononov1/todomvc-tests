debea375e078d53c2a9c73e91d3260c4e7581090
vlad <vladimirkon@gmail.com>
2020-10-02 18:01:29 -0700

    commented out Applitool related commands

--- cypress/integration/todomvc-visuals.spec.js
+++ cypress/integration/todomvc-visuals.spec.js
@@ -5,26 +5,26 @@ import * as todoPage from '../page-objects/todo-page'
 describe('visual validation', () => {
     before(() => todoPage.navigate())
 
-    beforeEach(() => cy.eyesOpen({appName: 'TAU TodoMVC', batchName: 'TAU TodoMVC Hey!',
-    browser: [
-        {name: 'chrome', width: 1024, height: 768},
-        {name: 'chrome', width: 800, height: 600},
-        {name: 'firefox', width: 1024, height: 768},
-        {deviceName: 'iPhone X'},
-    ]}))
+    // beforeEach(() => cy.eyesOpen({appName: 'TAU TodoMVC', batchName: 'TAU TodoMVC Hey!',
+    // browser: [
+    //     {name: 'chrome', width: 1024, height: 768},
+    //     {name: 'chrome', width: 800, height: 600},
+    //     {name: 'firefox', width: 1024, height: 768},
+    //     {deviceName: 'iPhone X'},
+    // ]}))
 
-    afterEach(() => cy.eyesClose())
+    // afterEach(() => cy.eyesClose())
 
     it('should look good', () => {
-        cy.eyesCheckWindow('empty todo list')
+        // cy.eyesCheckWindow('empty todo list')
 
         todoPage.addTodo('Clean room')
         todoPage.addTodo('Learn JavaScript')
 
-        cy.eyesCheckWindow('two todos')
+        // cy.eyesCheckWindow('two todos')
 
         todoPage.toggleTodo(0)
         
-        cy.eyesCheckWindow('mark as completed')
+        // cy.eyesCheckWindow('mark as completed')
     })
 })
